dfb83804d490f2968228d8a042b6a412422931aa
Mandy Choi <hello@mandychoi.com.au>
2016-10-22 16:45:22 +0100

    process turns until Quit

--- src/com/lessgame/ai/gamer.clj
+++ src/com/lessgame/ai/gamer.clj
@@ -4,6 +4,7 @@
 (def ORDER_OF_PLAY [:yellow :black :white :red])
 
 (defn- play-turn []
+  (println "b1b2")
   "b1b2")
 
 (defn- watch []
@@ -14,23 +15,16 @@
 (defn my-turn? [{:keys [player]} current-turn]
   (= player current-turn))
 
-(defn- next-move [state player]
+(defn- process-turn [state player]
   (if (my-turn? state player)
     (play-turn)
     (watch)))
-
-(defn- process-turn [state player]
-  (let [move (next-move state player)]
-    (when-not (nil? move)
-      move)))
 
 (defn start-playing []
   (let [board-str   (read-line)
         player-str  (read-line)
         state       (gr/read-game board-str player-str)]
 
-    (when-not (nil? (process-turn state :yellow))
-      (when-not (nil? (process-turn state :black))
-        (when-not (nil? (process-turn state :white))
-          (when-not (nil? (process-turn state :red))
-            (process-turn state :yellow)))))))
+    (loop [turns  (cycle ORDER_OF_PLAY)]
+      (when (process-turn state (first turns))
+        (recur (rest turns))))))
--- test/com/lessgame/ai/gamer_test.clj
+++ test/com/lessgame/ai/gamer_test.clj
@@ -2,22 +2,51 @@
   (:require [com.lessgame.ai.gamer :as g]
             [midje.sweet :refer :all]))
 
-(fact "yellow makes the first move of the game"
-      (g/start-playing) => nil
-      (provided
-        (read-line) =streams=> ["0000" "Yellow" "Quit"] :times 3))
+(facts ""
+ (let [board "0000"
+       yellow-move anything
+       black-move anything
+       white-move anything
+       red-move anything
+       quit "Quit"]
+   (fact "yellow makes the first move of the game"
+         (g/start-playing) => nil
+         (provided
+           (println "b1b2") => anything :times 1
+           (read-line) =streams=> [board "Yellow"
+                                   quit] :times 3))
 
-(fact "black makes the second move of the game"
-      (g/start-playing) => nil
-      (provided
-        (read-line) =streams=> ["0000" "Black" anything "Quit"] :times 4))
+   (fact "black makes the second move of the game"
+         (g/start-playing) => nil
+         (provided
+           (read-line) =streams=> [board "Black"
+                                   yellow-move
+                                   quit] :times 4))
 
-(fact "white makes the third move of the game"
-      (g/start-playing) => nil
-      (provided
-        (read-line) =streams=> ["0000" "White" anything anything "Quit"] :times 5))
+   (fact "white makes the third move of the game"
+         (g/start-playing) => nil
+         (provided
+           (read-line) =streams=> [board "White"
+                                   yellow-move
+                                   black-move
+                                   quit] :times 5))
 
-(fact "white makes the third move of the game"
-      (g/start-playing) => nil
-      (provided
-        (read-line) =streams=> ["0000" "Red" anything anything anything "Quit"] :times 6))
+   (fact "red makes the fourth move of the game"
+         (g/start-playing) => nil
+         (provided
+           (read-line) =streams=> [board "Red"
+                                   yellow-move
+                                   black-move
+                                   white-move
+                                   quit] :times 6))
+
+   (fact "is yellow's turn again after 3 moves have been played"
+         (g/start-playing) => nil
+         (provided
+           (println "b1b2") => anything :times 2
+           (read-line) =streams=> [board "Yellow"
+                                   yellow-move
+                                   black-move
+                                   white-move
+                                   red-move
+                                   quit]))))
